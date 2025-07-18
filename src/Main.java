6d37232a3bf6cfdea1b40589f3de29b7c4bb8c57
Karen <karenlbarcelos@hotmail.com>
2025-07-18 09:22:32 -0300

    updated: main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um CEP: ");
+        var leitura = scanner.nextLine();
+
+        var endereco = "https://viacep.com.br/ws/"+ leitura + "/json/";
+
+                HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        //usando o .send o programa espera a resposta antes de continuar, pode quebrar entao eh obrigatorio o uso do throws
+
+        String json = response.body();
+        System.out.println(json);
     }
 }
