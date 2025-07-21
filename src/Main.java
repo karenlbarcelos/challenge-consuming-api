a7230c13337e37b6c6830a04bcd6393b65d84625
Karen <karenlbarcelos@hotmail.com>
2025-07-20 23:03:49 -0300

    updated: adding clean code concepts

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import client.ViaCepClient;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -13,16 +15,20 @@ public class Main {
         System.out.println("Digite um CEP: ");
         var leitura = scanner.nextLine();
 
+
         var endereco = "https://viacep.com.br/ws/"+ leitura + "/json/";
 
-                HttpClient client = HttpClient.newHttpClient();
+        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         //usando o .send o programa espera a resposta antes de continuar, pode quebrar entao eh obrigatorio o uso do throws
 
+        ViaCepClient endereco = new ViaCepClient(leitura);
         String json = response.body();
         System.out.println(json);
+
+
     }
 }
